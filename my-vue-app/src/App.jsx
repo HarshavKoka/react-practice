5c4486746edee86589895571c40628eea5cb9544
HarshavKoka <kokaharsha2203@gmail.com>
2024-11-28 23:56:02 +0530

    my first react project

--- my-vue-app/src/App.jsx
+++ my-vue-app/src/App.jsx
@@ -4,18 +4,18 @@ import './App.css'
 export default function App() {
   const [count, setCount] = React.useState(0)
   function add() {
-    setCount(count + 1)
+    setCount(count + 2)
 
   } function sub() {
-    setCount(count - 1)
+    setCount(count - 2)
   }
   return (
     <div className="counter">
-      <button className="counter--minus" onClick={sub}>-</button>
+      <button className="counter--minus" onClick={sub}>-2</button>
       <div className="counter--count">
         <h1>{count}</h1>
       </div>
-      <button className="counter--plus" onClick={add}>+</button>
+      <button className="counter--plus" onClick={add}>+2</button>
     </div>
   )
 }
